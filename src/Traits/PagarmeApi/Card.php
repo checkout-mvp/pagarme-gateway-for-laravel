6e4a7eb2adda3d4f10d98505bb0a5c595ef83ace
Humberto Martins <humberto@consilio.com.br>
2022-02-07 15:32:52 -0300

    feat: improving card

--- src/Traits/PagarmeApi/Card.php
+++ src/Traits/PagarmeApi/Card.php
@@ -2,6 +2,7 @@
 namespace MartinsHumberto\PagarmeGateway\Traits\PagarmeApi;
 
 use MartinsHumberto\PagarmeGateway\Rules\CardValidation;
+use RuntimeException;
 
 trait Card
 {
@@ -9,6 +10,11 @@ trait Card
     {
         $this->validate($data, CardValidation::rules($this->version));
 
+        if ($this->version === '2019-09-01') {
+            throw new RuntimeException(
+                "Not implemented yet."
+            );
+        }
         if ($this->version === 'stable') {
             return $this->client->getTokens()->createToken($data);
         }
